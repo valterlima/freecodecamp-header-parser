f77793197be792766c21063d85b5684e51915205
Valter Lima Mesquita <limavalter@gmail.com>
2016-02-19 14:49:12 -0300

    using x-forwarded-for

--- controllers/headerController.js
+++ controllers/headerController.js
@@ -3,7 +3,8 @@ var useragent = require('useragent');
 function parseHeader(req){
 	var agent = useragent.parse(req.headers['user-agent']);
 	var software = agent.os.toString();
-	var ip = req.connection.remoteAddress
+	var ip = req.headers['x-forwarded-for'] || 
+     req.connection.remoteAddress;
 	var lang = req.headers["accept-language"];
 	return {
 		'ipaddress'	:ip,
